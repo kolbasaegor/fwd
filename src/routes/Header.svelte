b3db68fe5f06309fb9539daa83eb0a4a2c865795
kolbasaegor <40235209+kolbasaegor@users.noreply.github.com>
2023-03-20 20:01:35 +0300

    homework 5

---
+++ src/routes/Header.svelte
@@ -0,0 +1,88 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">About</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/xkcd' ? 'page' : undefined}>
+				<a href="/xkcd">XKCD</a>
+			</li>
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
+</header>
+<div class="hline"></div>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+
+	nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	path {
+		fill: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--color-theme-1);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 1rem;
+		color: white;
+		font-weight: 700;
+		font-size: 1.6rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		background-color: rgb(147, 0, 180);
+	}
+
+</style>
