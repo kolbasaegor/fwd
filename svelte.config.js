4456d23bd3d3ac0270949c0f0eff8f6ad24459ec
kolbasaegor <40235209+kolbasaegor@users.noreply.github.com>
2023-03-20 20:14:23 +0300

    svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,9 +14,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/fwd',
-		  }
+        paths: {
+            base: dev ? '' : '/fwd',
+        }
 	}
 };
 
