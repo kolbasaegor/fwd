8167b707387715a3b74c065ccb9b6481bfb0f865
kolbasaegor <40235209+kolbasaegor@users.noreply.github.com>
2023-04-03 15:26:47 +0000

    Deploying to gh-pages from @ kolbasaegor/fwd@eecd67541f1c37c8f9535808f0cc0e7266f018d9 🚀

---
+++ _app/immutable/entry/_page.svelte.c071feb3.js
@@ -0,0 +1,15 @@
+import{S as Y,i as Z,s as $,k as i,a as I,q as n,J as tt,l as r,h as e,c as x,m as o,r as s,n as g,D as t,b as W,H as z}from"../chunks/index.6b56aa9b.js";function et(X){let d,C,a,v,M,S,_,U,A,u,L,p,P,V,h,j,B,l,b,D,H,E,J,T,k,w;return{c(){d=i("meta"),C=I(),a=i("div"),v=i("h1"),M=n("Egor Stroganov"),S=I(),_=i("p"),U=n(`My name is Egor. I am 3rd yeat student at Innopolis University, sit amet consectetur adipisicing
+		elit. Voluptas debitis minima a earum. Corporis incidunt cumque deleniti laboriosam tenetur illo
+		aperiam veniam reprehenderit. Consequuntur voluptatum necessitatibus quas repellendus eligendi
+		dolore.`),A=I(),u=i("p"),L=n(`Useful links:
+		`),p=i("a"),P=n("tg"),V=n(` |
+		`),h=i("a"),j=n("github"),B=n(`
+	My projects:
+	`),l=i("ul"),b=i("li"),D=n("Bebro4ka - description"),H=I(),E=i("li"),J=n("simbochka - description"),T=I(),k=i("li"),w=n("tiger - description"),this.h()},l(c){const y=tt("svelte-1gogdsr",document.head);d=r(y,"META",{name:!0,content:!0}),y.forEach(e),C=x(c),a=r(c,"DIV",{id:!0});var m=o(a);v=r(m,"H1",{});var F=o(v);M=s(F,"Egor Stroganov"),F.forEach(e),S=x(m),_=r(m,"P",{});var G=o(_);U=s(G,`My name is Egor. I am 3rd yeat student at Innopolis University, sit amet consectetur adipisicing
+		elit. Voluptas debitis minima a earum. Corporis incidunt cumque deleniti laboriosam tenetur illo
+		aperiam veniam reprehenderit. Consequuntur voluptatum necessitatibus quas repellendus eligendi
+		dolore.`),G.forEach(e),A=x(m),u=r(m,"P",{});var q=o(u);L=s(q,`Useful links:
+		`),p=r(q,"A",{href:!0,target:!0});var K=o(p);P=s(K,"tg"),K.forEach(e),V=s(q,` |
+		`),h=r(q,"A",{href:!0,target:!0});var N=o(h);j=s(N,"github"),N.forEach(e),q.forEach(e),B=s(m,`
+	My projects:
+	`),l=r(m,"UL",{});var f=o(l);b=r(f,"LI",{});var O=o(b);D=s(O,"Bebro4ka - description"),O.forEach(e),H=x(f),E=r(f,"LI",{});var Q=o(E);J=s(Q,"simbochka - description"),Q.forEach(e),T=x(f),k=r(f,"LI",{});var R=o(k);w=s(R,"tiger - description"),R.forEach(e),f.forEach(e),m.forEach(e),this.h()},h(){document.title="About",g(d,"name","description"),g(d,"content","Svelte demo app"),g(p,"href","https://t.me/doctoregor"),g(p,"target","_blank"),g(h,"href","https://github.com/kolbasaegor"),g(h,"target","_blank"),g(a,"id","bio")},m(c,y){t(document.head,d),W(c,C,y),W(c,a,y),t(a,v),t(v,M),t(a,S),t(a,_),t(_,U),t(a,A),t(a,u),t(u,L),t(u,p),t(p,P),t(u,V),t(u,h),t(h,j),t(a,B),t(a,l),t(l,b),t(b,D),t(l,H),t(l,E),t(E,J),t(l,T),t(l,k),t(k,w)},p:z,i:z,o:z,d(c){e(d),c&&e(C),c&&e(a)}}}class it extends Y{constructor(d){super(),Z(this,d,null,et,$,{})}}export{it as default};
